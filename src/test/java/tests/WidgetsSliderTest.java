a2fce6ad787cea755be3c19f76a1d688f8d648e4
knackiuz <knackiuz@gmail.com>
2026-02-19 23:47:22 +0200

    updated: pom

--- src/test/java/tests/WidgetsSliderTest.java
+++ src/test/java/tests/WidgetsSliderTest.java
@@ -21,6 +21,6 @@ public class WidgetsSliderTest extends BaseTest {
                 .setSliderValue(sliderTargetValue)
                 .checkSliderValue(sliderTargetValue);
 
-        page.pause();
+        //page.pause();
     }
 }
